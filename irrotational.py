still checking rotation, fixing FEM offsets

irrotational.py
@@ -479,12 +479,10 @@ def phasediagram():
   A static function currently to produce a 'phase diagram' of stability
   '''
   gvals = 10 ** np.linspace( -2., 3., 20. )
-  
-  
-  
-  
-  
-  
+  Gvals = 10 ** np.linspace( -2., 3., 20. )
+  for g in gvals:
+    for G in Gvals:
+      
   
   
   

scripts/initials.py
@@ -83,8 +83,8 @@ def twoblobs(x        ,
   sig1 = 1.
   pi = np.pi
   sig2 = 1.
-  psi1 = 1.0/np.sqrt(2*pi*sig1**2) * np.exp(-((X-xo1)**2 + Y**2.) / (2*sig1**2)) 
-  psi2 = 1.0/np.sqrt(2*pi*sig2**2) * np.exp(-((X-xo2)**2 + Y**2.) / (2*sig2**2))
+  psi1 = 1.0/np.sqrt(2*pi*sig1**2) * np.exp(-((Y-xo1)**2 + X**2.) / (2*sig1**2)) 
+  psi2 = 1.0/np.sqrt(2*pi*sig2**2) * np.exp(-((Y-xo2)**2 + X**2.) / (2*sig2**2))
   psi = psi1 + psi2
   norm = sum(sum(psi*psi.conjugate())) * abs(x[1]-x[0]) * abs(y[1]-y[0])
   psi = psi / np.sqrt(norm)

scripts/rotate.py
@@ -449,7 +449,8 @@ class Bose:
     '''
     assert order in [ 3, 5, 7, 9, 11, 13 ],\
       'Only order values contained in [ 3, 5, 7, 9, 11, 13 ] are implemented\
-     \nthus far, please ensure you are have the order type correct also.'
+     \nthus far.'
+    
     
     if order == 3:
       a = np.array( [ 0., 0., 0., 0., 0., -1., 0., 1., 0., 0., 0., 0., 0. ] )
@@ -486,15 +487,19 @@ class Bose:
     for jj in np.where( [ii != 0 for ii in a] )[0]:
       #Iterate over the diagonals in a that are non-zero
       #offset of jj from array centred
-      off = jj - np.ceil( jj/2. )
+      off = jj - np.ceil( len( a ) /2. ) + 1.
       data.append( a[jj] * np.ones( [ 1, self.npt ** 2 ] ) )
       diag.append( off )
       
       data.append( a[jj] * np.ones( [ 1, self.npt ** 2 ] ) )
-      diag.append( self.npt + off )
-    
+      diag.append( off * self.npt )
+      
+
+    print diag
+    print data
     data = np.vstack(data) #put the data in rank 2 array
-    
+    test = spdiags( data, diag, self.npt ** 2., self.npt ** 2. )
+    print test.getrow(0),'\n\n', test.getrow(1)
     return self.rot * spdiags( data, diag, self.npt ** 2., self.npt ** 2. )
   
 ################################################################################